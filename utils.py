1518432aa1934d200afff560716142284d55b1eb
Angelina Theodosi <angelina.theodosi13@gmail.com>
2026-04-20 00:04:00 +0300

    smote oversampler added

--- utils.py
+++ utils.py
@@ -3,11 +3,12 @@ import pandas as pd
 from tdc.single_pred import Tox
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score, average_precision_score
 from xgboost import XGBClassifier
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import Pipeline as ImbPipeline
 
 # Load dataset from TDC and perform scaffold split
 def load_tdc_dataset(tdc_name):
@@ -28,13 +29,14 @@ def get_feature_columns(df: pd.DataFrame, label_col="Y"):
     return feat_cols
 
 # Train and evaluate a model
-def run_experiment(df: pd.DataFrame, model_type="logistic"):
+def run_experiment(df: pd.DataFrame, model_type="logistic", use_smote=False):
     label_col = "Y"
     feat_cols = get_feature_columns(df, label_col=label_col)
 
     if "split" not in df.columns:
         raise ValueError("Split column not found in DataFrame.")
 
+    
     # Isolate data subsets
     train_df = df[df["split"] == "train"].copy()
     valid_df = df[df["split"] == "valid"].copy()
@@ -75,7 +77,10 @@ def run_experiment(df: pd.DataFrame, model_type="logistic"):
     else:
         raise ValueError(f"Unknown model_type: {model_type}")
 
-    clf = Pipeline(steps=steps)
+    clf = ImbPipeline(steps=steps)
+    if use_smote:
+        steps.insert(-1, ("smote", SMOTE(random_state=42)))
+
     clf.fit(X_train, y_train)
 
     # Predict probabilities for evaluation
