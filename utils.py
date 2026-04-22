2db58a402a5f746627e53122870f89f01761b9d4
Angelina Theodosi <angelina.theodosi13@gmail.com>
2026-04-22 15:24:17 +0300

    changed imb pipeline to sklearn's pipeline

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score, average_precision_score
 from xgboost import XGBClassifier
-from imblearn.pipeline import Pipeline as ImbPipeline
+from sklearn.pipeline import Pipeline
 
 # Load dataset from TDC and perform scaffold split
 def load_tdc_dataset(tdc_name):
@@ -35,7 +35,6 @@ def run_experiment(df: pd.DataFrame, model_type="logistic"):
     if "split" not in df.columns:
         raise ValueError("Split column not found in DataFrame.")
 
-    
     # Isolate data subsets
     train_df = df[df["split"] == "train"].copy()
     valid_df = df[df["split"] == "valid"].copy()
